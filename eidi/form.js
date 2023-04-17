ad11eed9c77dc6a13eb6c21238a266f94ecd9f11
shameeri312 <114741290+shameeri312@users.noreply.github.com>
2023-04-17 13:47:56 +0500

    Add files via upload

--- eidi/form.js
+++ eidi/form.js
@@ -1,9 +1,17 @@
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username != "tayyba" && username != "Tayyba") {
-        alert("ERROR!! LOGING IN");
+    var nextside = document.getElementById("nextside");
+    var welcome = document.getElementById("welcome");
+    var headline = document.getElementById("headline");
+    var login = document.getElementById("login");
+    if (username == "shameer" && password == "tayyba") {
+        nextside.style.opacity = "1";
+        login.style.opacity = "0";
+        login.style.visibility = "hidden";
+        login.style.transition = "all 1s";
+        document.getElementById("lg").style.display = "inline";
     } else {
-        alert("LOGGED IN");
+        alert("incorrect login");
     }
 }
